0174accab208a2c2030b403feccc5de8adbabed2
Arley Novais <nascimentoarle@gmail.com>
2020-10-13 21:39:35 -0400

    Add .

--- app/src/main/java/com/br/arley/sact/model/Project.java
+++ app/src/main/java/com/br/arley/sact/model/Project.java
@@ -1,19 +1,50 @@
 package com.br.arley.sact.model;
 
-public class Project {
-    private String name;
-    private String classroom;
-    private String course;
-    private String[] members;
+import com.google.gson.annotations.SerializedName;
 
-    public Project() {
+public class Project {
+
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    @SerializedName("occupation_area")
+    private String occupationArea;
+
+    private String classroom;
+
+    private String members;
+
+    private String observations;
+
+    @SerializedName("image_url")
+    private String imageURL;
+
+    @SerializedName("created_at")
+    private String createdAt;
+
+    @SerializedName("updated_at")
+    private String updatedAt;
+
+    public Project(){
+
     }
 
-    public Project(String name, String classroom, String course, String[] members) {
+    public Project(String name, String classroom, String occupationArea, String members){
         this.name = name;
         this.classroom = classroom;
-        this.course = course;
+        this.occupationArea = occupationArea;
         this.members = members;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -24,6 +55,22 @@ public class Project {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOccupationArea() {
+        return occupationArea;
+    }
+
+    public void setOccupationArea(String occupationArea) {
+        this.occupationArea = occupationArea;
+    }
+
     public String getClassroom() {
         return classroom;
     }
@@ -32,19 +79,59 @@ public class Project {
         this.classroom = classroom;
     }
 
-    public String[] getMembers() {
+    public String getMembers() {
         return members;
     }
 
-    public void setMembers(String[] members) {
+    public void setMembers(String members) {
         this.members = members;
     }
 
-    public String getCourse() {
-        return course;
+    public String getObservations() {
+        return observations;
     }
 
-    public void setCourse(String course) {
-        this.course = course;
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", occupationArea='" + occupationArea + '\'' +
+                ", classroom='" + classroom + '\'' +
+                ", members='" + members + '\'' +
+                ", observations='" + observations + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                ", updatedAt='" + updatedAt + '\'' +
+                '}';
     }
 }
--- app/src/main/java/com/br/arley/sact/model/SactServer.java
+++ app/src/main/java/com/br/arley/sact/model/SactServer.java
@@ -5,16 +5,18 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface SactServer {
 
-    @GET("/posts")
-    Call<List<Project>> getProjects();
+    @GET("/avaliations/{evaluator_id}")
+    Call<List<Avaliation>> getAvaliationsByEvaluatorId(
+            @Path("evaluator_id") String evaluatorId,
+            @Header("Authorization") String token);
 
     @POST("/evaluators/sessions")
-    Call<AuthData> authenticateEvaluatorByEmail(@Body String email);
-
+    Call<AuthData> authenticateEvaluatorByEmail(@Body Email email);
 
 }
